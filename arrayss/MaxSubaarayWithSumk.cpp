29e258a02e667741e2eb1e15a4fd40882e9e48e5
cherishjainn <cherishjain56@gmail.com>
2026-06-29 01:00:53 +0530

    Update MaxSubaarayWithSumk.cpp

--- arrayss/MaxSubaarayWithSumk.cpp
+++ arrayss/MaxSubaarayWithSumk.cpp
@@ -19,8 +19,6 @@ int longestSubarray(vector<int>& arr, int k) {
     return ans;
 }
 
-/*
-
 Maan lo array hai:
 
 [1, 2, 1, 1, 1, 3]
@@ -39,3 +37,38 @@ Agar sum > K ho jaye, to left ko aage badha kar sum kam karo.
 Agar sum == K, to window length update karo.
 
     */
+
+##### PREFIX SUM METHOD
+    
+    int longestSubarray(vector<int>& arr, int k) {
+    unordered_map<long long, int> mp;
+    long long sum = 0;
+    int maxi = 0;
+
+    for(int i = 0; i < arr.size(); i++) {
+        sum += arr[i];
+
+        if(sum == k)
+            maxi = max(maxi, i + 1);
+
+        if(mp.find(sum - k) != mp.end())
+            maxi = max(maxi, i - mp[sum - k]);
+
+        if(mp.find(sum) == mp.end())
+            mp[sum] = i;   // store first occurrence only
+    }
+
+    return maxi;
+}
+
+/* yaha ek important baat h 
+* for sum == k directly check the condition 
+* if 0 is present in array may decrase lenght ( ex. [2,0,0,3]  so put condition 
+ --if(mp.find(sum) == mp.end())
+            mp[sum] = i;  
+*/
+
+
+
+
+
