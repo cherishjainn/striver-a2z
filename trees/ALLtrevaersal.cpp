283da65663bde6971355186d5de63ec2949e3439
cherishjainn <cherishjain56@gmail.com>
2026-06-29 11:37:43 +0530

    Update ALLtrevaersal.cpp

--- trees/ALLtrevaersal.cpp
+++ trees/ALLtrevaersal.cpp
@@ -1,4 +1,40 @@
- void pre(TreeNode* root,vector<int> &ans){
+## LEVEL ORDER TRAVERSAL 
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if (root == NULL)   
+            return ans;  /* {}*/
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node->val);
+
+                if (node->left)
+                    q.push(node->left);
+
+                if (node->right)
+                    q.push(node->right);
+            }
+
+            ans.push_back(level);
+        }
+
+        return ans;
+    }
+
+
+
+## PREORDER TRAVERSAL
+void pre(TreeNode* root,vector<int> &ans){
         if(root == NULL) return ;
         ans.push_back(root->val);
         pre(root->left,ans);
@@ -11,7 +47,7 @@
         return ans ;
     }
 
-
+## INORDER TRAVERSAL
  void inorder(TreeNode* root,vector<int> &ans){
         if(root == NULL) return ;
         inorder(root->left,ans);
@@ -24,7 +60,7 @@
         return ans ;
     }
     
-    
+   ## POSTORDER TRAVERSAL 
   void postorder(TreeNode* root,vector<int> &ans){
         if(root == NULL) return ;
         postorder(root->left,ans);
@@ -38,3 +74,4 @@
         postorder(root,ans);
         return ans ;
     }  
+
