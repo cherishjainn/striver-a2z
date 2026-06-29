76b34401d04ca5583b558e83cc91138937e96d93
cherishjainn <cherishjain56@gmail.com>
2026-06-29 12:04:42 +0530

    Update Iterative_Preorder.cpp

--- trees/Iterative_Preorder.cpp
+++ trees/Iterative_Preorder.cpp
@@ -12,3 +12,13 @@ vector<int> preorderTraversal(TreeNode* root) {
         }   
         return ans ;                    
     }
+
+LOGIC ==>
+- 1. Root ko stack me daalo.
+2. Stack se top most node nikalo. =>sabse top most ko nikalenge kyoki vo left vala node h (jo ki aghe ki subtree ke liye head h)
+3. Usko answer me daalo.
+4. Pehle right push karo, phir left push karo => kyoki hame ( root left right ) chaiye sabse pahele head push krenge fir uske bas usko pop krte hue  
+                                                right - left push krenge aur sath me head ko ans vector me store bhi krte jayenge aur jaise abhi 
+                                                right-left store hue h jab next bar me top nikalenge tho vo left vala ayega top pe hoga tho usko store krlenge kyoki vo  head of next subtree hai  !!!!
+
+        
