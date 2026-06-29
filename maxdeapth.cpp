3b7e7e3341689effae16c3668e24c66b3231da82
cherishjainn <cherishjain56@gmail.com>
2026-06-29 22:06:03 +0530

    Update maxdeapth.cpp

--- maxdeapth.cpp
+++ maxdeapth.cpp
@@ -4,3 +4,5 @@
         int rightheight = maxDepth(root->right);
         return 1+max(leftheight,rightheight);
     }
+
+** jab recusrion likh rha h tho soch mera function muzhe vo required chij de dega ye man ke aage bad aur bas base condtiton likh de 
