3cb54543e1353ca4e7d5aec8d13c37105d1b98da
cherishjain <cherishjain56@gmail.com>
2026-07-05 17:10:01 +0530

    Update roottonodepath.cpp

--- trees/roottonodepath.cpp
+++ trees/roottonodepath.cpp
@@ -1,3 +1,14 @@
+PATH FROM ROOT TO ANY NODE 
+whenever doing recursion think that you aim is current node means consider you ot what  you want from further now adjust code like what you have to return to above means when you call function again in function for recursio assume you got a  prefect desired asnwer from function now write code such that what you want to do with current elemnt or what you want above function should receive from you
+note====>>>
+
+    
+VERY IMPORTANT : since there is only one path of root to node you did not need to move in left then pop then move in right then pop
+you can use true false concept and if you revceived true means you find path
+ if you get false then it means at evry step from end to that point you must have got false 
+so we have written a line of pop back and return false after doing if condition did not work !!!! 
+
+
 bool getPath(TreeNode* root, int x, vector<int>& path) {
     if (root == NULL) return false;
 
@@ -12,6 +23,7 @@ bool getPath(TreeNode* root, int x, vector<int>& path) {
     if (getPath(root->left, x, path) || getPath(root->right, x, path))
         return true;
 
+    // let say you got true from 
     // Backtrack: remove current node
     path.pop_back();
 
